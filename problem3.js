735b0fef37a3e0a98d143be537dea8f908296882
hammad <hammadafroz007@gmail.com>
2024-04-11 21:27:02 +0530

    Solved problem 3

---
+++ problem3.js
@@ -0,0 +1,8 @@
+// problem3.js
+
+function sortCarModelsAlphabetically(inventory) {
+    const sortedModels = inventory.map(car => car.car_model).sort();
+    return sortedModels;
+}
+
+module.exports = sortCarModelsAlphabetically;
---
+++ testProblem3.js
@@ -0,0 +1,8 @@
+// test/testProblem3.js
+
+const sortCarModelsAlphabetically = require('./problem3');
+const inventory = require('./inventory');
+
+// Test case
+const sortedModels = sortCarModelsAlphabetically(inventory);
+console.log(sortedModels);
